27af24eedec4e5da62572a0270b0c7d0a02e41c5
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2017-10-16 23:44:19 -0300

    feat(interactions): first approach for simulate interactions for change the generated classes by angular form based on the given component definition

--- src/angularjsSnapshotFormat.js
+++ src/angularjsSnapshotFormat.js
@@ -17,7 +17,42 @@ const HTMLNormalizer = (element) => {
 		});
 	});
     return element[0];
-}
+};
+
+const touchElement = (el) => {
+    el.focus();
+    el.blur();
+};
+const doElementInteraction = (el, field) => {
+    Object.keys(field).forEach((property) => {
+        switch(property) {
+            case "$touched":
+                if(field.$touched) {
+                    touchElement(el);
+                }
+                break;
+        }
+    });
+};
+const objectTraverse = (obj, path) => path.split(".").slice(1).reduce((a,b) => a[b], obj);
+const checkInteractions =  (element, $ctrl) => {
+    const formElement = element[0].parentNode.querySelector("form");
+
+    if(formElement) {
+        const formName = (/\.(\w+)$/g).exec(formElement.name)[1];
+        const formController = objectTraverse($ctrl, formElement.name);
+        const namedElements = formElement.querySelectorAll("[name]");
+
+        if(formController) {
+            Array.from(namedElements).forEach((el) => {
+                const formField = formController[el.name];
+                if(formField) {
+                    doElementInteraction(el, formField, $ctrl);
+                }
+            });
+        }
+    }
+};
 
 export const angularjsSnapshotFormat = ({ template, $ctrl }) => {
     const scope = $rootScope.$new();
@@ -25,10 +60,13 @@ export const angularjsSnapshotFormat = ({ template, $ctrl }) => {
 
     let el = $compile(template)(scope);
     Object.assign(scope.$ctrl, $ctrl);
+
+    checkInteractions(el, $ctrl);
+
     scope.$digest();
 
     removeHTMLComments(el[0].childNodes);
     el = HTMLNormalizer(el);
 
     return el.outerHTML;
-}
+};
